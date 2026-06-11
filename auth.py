e534f6918707a4a0610ccda88180aa55b1088255
KayIssy54 <mwongerajoy4@gmail.com>
2026-06-11 11:50:16 +0300

    Add function to load users from JSON file

--- auth.py
+++ auth.py
@@ -2,3 +2,10 @@ import json
 from models.user import User
 
 USERS_FILE = "data/users.json"
+
+def load_users():
+    try:
+        with open(USERS_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
