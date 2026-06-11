054bf74a4bc9467e8b1943928358d1f4cb0a43f6
KayIssy54 <mwongerajoy4@gmail.com>
2026-06-11 10:59:05 +0300

    Add password hashing functionality

--- user.py
+++ user.py
@@ -1,5 +1,10 @@
+import hashlib
+
 class User:
     def __init__(self, username, password, role="member"):
         self.username = username
-        self.password = password
+        self.password = self.hash_password(password)
         self.role = role
+
+    def hash_password(self,password):
+        return hashlib.sha256(password.encode()).hexdigest()   
